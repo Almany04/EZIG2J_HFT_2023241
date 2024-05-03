3c9e111747dee95054de420014b86dd27064f727
Almany04 <bencealmas@gmail.com>
2024-05-03 17:23:45 +0200

    Az abstract repository létrehozása

---
+++ EZIG2J_HFT_2023241.Repository/Repository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EZIG2J_HFT_2023241.Repository
+{
+    internal class Repository
+    {
+    }
+}
