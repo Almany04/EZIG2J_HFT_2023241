90a21c4fde8c2f0037336952ae43af6f695c6c8c
Almany04 <bencealmas@gmail.com>
2024-05-03 17:24:11 +0200

    Feltöltése a Repositorynak

--- EZIG2J_HFT_2023241.Repository/Repository.cs
+++ EZIG2J_HFT_2023241.Repository/Repository.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace EZIG2J_HFT_2023241.Repository
 {
-    internal class Repository
+    public abstract class Repository<T> : IRepository<T> where T : class
     {
+        protected EmployeeDbContext ctx;
+        public Repository(EmployeeDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public void Delete(int id)
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+
+        public abstract T Read(int id);
+        public abstract void Update(T item);
+
     }
 }
