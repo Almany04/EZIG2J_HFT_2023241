fa198c1207e3b6407605a0e76d51b1f49b7d7d99
Almany04 <bencealmas@gmail.com>
2024-05-03 17:14:48 +0200

    DbSet-ek feltöltése

--- EZIG2J_HFT_2023241.Repository/EmployeeDbContext.cs
+++ EZIG2J_HFT_2023241.Repository/EmployeeDbContext.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using EZIG2J_HFT_2023241.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace EZIG2J_HFT_2023241.Repository
 {
-    public class Class1
+    internal class EmployeeDbContext :DbContext
     {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<ProjectAssignment> ProjectAssignments { get; set; }
+
     }
 }
