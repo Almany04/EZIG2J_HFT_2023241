823d46f874034fa65a26e07485b2a26b39d091ae
Almany04 <bencealmas@gmail.com>
2024-05-08 01:01:21 +0200

    ProjectLogic elkészülte az Interfésszel együtt

---
+++ EZIG2J_HFT_2023241.Logic/IProjectLogic.cs
@@ -0,0 +1,14 @@
+﻿using EZIG2J_HFT_2023241.Models;
+using System.Linq;
+
+namespace EZIG2J_HFT_2023241.Logic
+{
+    public interface IProjectLogic
+    {
+        void Create(Project item);
+        void Delete(int id);
+        Project Read(int id);
+        IQueryable<Project> ReadAll();
+        void Update(Project item);
+    }
+}
---
+++ EZIG2J_HFT_2023241.Logic/ProjectLogic.cs
@@ -0,0 +1,46 @@
+﻿using EZIG2J_HFT_2023241.Models;
+using EZIG2J_HFT_2023241.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EZIG2J_HFT_2023241.Logic
+{
+    public class ProjectLogic : IProjectLogic
+    {
+        IRepository<Project> repo;
+
+        public ProjectLogic(IRepository<Project> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Project item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Project Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Project> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Project item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
