d03d6771960189f49a3288774eea2e7f15da89be
Almany04 <bencealmas@gmail.com>
2024-05-08 00:58:43 +0200

    DepartmentLogic elkészülte az Interfészel

---
+++ EZIG2J_HFT_2023241.Logic/DepartmentLogic.cs
@@ -0,0 +1,47 @@
+﻿using EZIG2J_HFT_2023241.Models;
+using EZIG2J_HFT_2023241.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EZIG2J_HFT_2023241.Logic
+{
+    public class DepartmentLogic : IDepartmentLogic
+    {
+        IRepository<Department> repo;
+
+        public DepartmentLogic(IRepository<Department> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Department item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Department Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Department> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Department item)
+        {
+            this.repo.Update(item);
+        }
+
+    }
+}
---
+++ EZIG2J_HFT_2023241.Logic/IDepartmentLogic.cs
@@ -0,0 +1,14 @@
+﻿using EZIG2J_HFT_2023241.Models;
+using System.Linq;
+
+namespace EZIG2J_HFT_2023241.Logic
+{
+    public interface IDepartmentLogic
+    {
+        void Create(Department item);
+        void Delete(int id);
+        Department Read(int id);
+        IQueryable<Department> ReadAll();
+        void Update(Department item);
+    }
+}
