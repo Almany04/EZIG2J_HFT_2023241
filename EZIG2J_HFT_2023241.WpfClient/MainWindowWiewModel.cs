dac2383401a901be50bc1d1389e5716f8dc277fd
Almany04 <bencealmas@gmail.com>
2024-11-12 23:05:55 +0100

    Kész van a Nevek listázása

---
+++ EZIG2J_HFT_2023241.WpfClient/MainWindowWiewModel.cs
@@ -0,0 +1,19 @@
+﻿using EZIG2J_HFT_2023241.Models;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EZIG2J_HFT_2023241.WpfClient
+{
+    public class MainWindowWiewModel
+    {
+        public RestCollection<Employee> employees { get; set; }
+        public MainWindowWiewModel()
+        {
+            employees = new RestCollection<Employee>("http://localhost:39574/", "employee");
+        }
+    }
+}
